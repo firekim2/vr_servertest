9e140e1386af9cb9a27ac6dd63b4863443cf2a51
Sungwoo <mar2019-24@Mar2019-24.local>
2019-05-08 16:16:35 +0900

    mend

--- app.js
+++ app.js
@@ -14,10 +14,12 @@ app.get('/sender', function(req, res) {
     res.sendFile(__dirname + '/test_sender.html');
 })
 
-var user_list = []
-
+var client_list = {}
+var console_list = {}
+var preset_device_id = {"PRESET_DEVICE_ID_1" : "client_1"}
+var client_unknown_id = 0;
 var upload_data = io.of('/upload_data').on('connection', function(socket) {
-
+var console_id = 0;
     //console.log(socket.id);
     //console.log(socket);
 
@@ -38,21 +40,25 @@ var upload_data = io.of('/upload_data').on('connection', function(socket) {
     socket.on('request-username', function(data){
         if(typeof(data) == "string") data = JSON.parse(data);
         var user_category = socket.user_category = data.user_category;
+        var device_name = socket.device_name = data.device_name;
         if(user_category == "client"){
-            if(user_list.indexOf(socket.id) === -1) user_list.push(socket.id);
+            if(!sokcet.id in client_list){
+                client_list[socket.id] = (device_name in preset_device_id) ? preset_device_id[device_name] : "client_unknown_id" + unknown_id++;
+            }
+        }
+        else{
+            if(!socket.id in console_list){
+                console_list[socket.id] = "console" + console_id++;
+            }
         }
         socket.join(user_category);
-        let user_id = (user_category == "client") ? user_list.indexOf(socket.id) : -10; // console : -10 , client : 0 ~ n
-        upload_data.to(socket.id).emit('sending-username', {"user_id" : user_id}); // only to sender
-        upload_data.to("console").emit('join-room', {"user_id" : user_id, "socket_id" : socket.id}); // update to console
-        console.log(user_id);
+        let user_id = (user_category == "client") ? client_list[socket.id] : console_list[socket.id];
+        upload_data.to(socket.id).emit('receiving-username', {"user_id" : user_id}); // only to sender
+        if(user_category == "client") upload_data.to("console").emit('join-room', {"user_id" : user_id, "socket_id" : socket.id}); // update to console
     })
 
     socket.on('disconnect', function(){
-        if(socket.rooms == "client"){
-            var user_id = user_list.indexOf(socket.id);
-            user_list[user_list.indexOf(socket.id)] = "out";
-        }
+        if(socket.rooms == "client") delete client_list[socket.id];
         upload_data.to("console").emit('leave-room', {"user_id" : user_id, "socket_id" : socket.id}); // update to console
     });
 
